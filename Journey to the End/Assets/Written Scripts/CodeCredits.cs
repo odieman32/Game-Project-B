e2075f83934d9bfe2be0193e48bc19351dd1c656
odieman32 <dettermanbrodie@gmail.com>
2024-10-14 16:18:48 -0400

    Added Pause Menu and Starting Heart Health

--- Journey to the End/Assets/Written Scripts/CodeCredits.cs
+++ Journey to the End/Assets/Written Scripts/CodeCredits.cs
@@ -5,5 +5,7 @@ using UnityEngine;
 public class CodeCredits : MonoBehaviour
 {
     //Written Script Credits: https://www.youtube.com/watch?v=U3sT-T5bKX4
+    //https://www.youtube.com/watch?v=MNUYe0PWNNs&t=351s
+    //https://www.youtube.com/watch?v=aUi9aijvpgs
 
 }
---
+++ Journey to the End/Assets/Written Scripts/PauseMenu.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
+
+public class PauseMenu : MonoBehaviour
+{
+    [SerializeField] GameObject pauseMenu;
+
+    public void Pause()
+    {
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0;
+    }
+
+    public void Home()
+    {
+        SceneManager.LoadScene(0);
+        Time.timeScale = 1;
+    }
+
+    public void Resume()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1;
+    }
+
+    public void Save()
+    {
+
+    }
+}
